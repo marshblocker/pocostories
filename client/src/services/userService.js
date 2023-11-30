54adebcd44607cdecc414ae1d7dcebdc7a3646b7
Gabriel Kenneth Marinas <gabrielkennethmarinas@gmail.com>
2023-12-01 03:19:58 +0800

    Rename user.service.js to userService.js

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
 
 const URL = 'http://localhost:4000';
+
+async function loginUser(username, password) {
+    try {
+        const credentials = {
+            'username': username,
+            'password': password
+        };
+    
+        let response = await axios.post(URL + '/login', { 'credentials': credentials });
+        let data = response.data;
+        return data;   
+    } catch (error) {
+        throw error;
+    }
+}
+
 async function registerUser(username, password) {
     try {
         const userInfo = {
@@ -17,5 +33,6 @@ async function registerUser(username, password) {
 }
 
 export {
+    loginUser,
     registerUser
 }
